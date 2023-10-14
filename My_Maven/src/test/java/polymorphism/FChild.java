b4c951c28f19897b90d785ca12add08b6fa3f609
Das <Das@Anu>
2023-10-14 18:52:50 +0530

    plymrphsm.

---
+++ My_Maven/src/test/java/polymorphism/FChild.java
@@ -0,0 +1,16 @@
+package polymorphism;
+
+public class FChild extends FParent {
+
+	public void print()
+	{
+		System.out.println("anusree");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FChild obj=new FChild();
+		obj.print();
+
+	}
+
+}
